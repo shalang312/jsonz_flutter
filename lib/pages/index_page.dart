72b3d014af50153ee876112e7306682725e12d1d
zhangliwen <zhangliwen@yodoo.net.cn>
2019-04-09 09:23:37 +0800

    简单实现底部栏 4.9

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,77 @@
+// 两种风格
+import 'package:flutter/material.dart';
+
+// 倾向于ios风格
+import 'package:flutter/cupertino.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'member_page.dart';
+import 'cart_page.dart';
+
+// 动态组件
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  // 底部栏组件集合
+  final List<BottomNavigationBarItem> bottomTbas = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text("首页"),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      title: Text("分类"),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      title: Text("购物车"),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      title: Text("会员中心"),
+    ),
+  ];
+
+  // 页面集合
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  // 索引
+  int currentIndex = 0;
+  // 当前选择页面
+  var currentPage;
+
+  // 默认页面
+  @override
+  void initState() {
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(244, 245, 1, 0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTbas,
+        onTap: (index) {
+          // 切换页面之后刷新状态
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
+      ),
+      body: currentPage,
+    );
+  }
+}
