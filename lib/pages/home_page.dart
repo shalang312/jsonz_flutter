75f6154513a40a9535874efe7e8d0dbdced553f2
zhangliwen <zhangliwen@yodoo.net.cn>
2019-04-11 20:58:15 +0800

    实现轮播图 4.11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jsonz_flutter/service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,17 +9,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String homePageContent = '正在获取数据';
-  @override
-  void initState() {
-    getHomePageContent().then((val){
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
+
+  // 有了banner之后，就不需要初始化状态了
+//  @override
+//  void initState() {
+//    getHomePageContent().then((val) {
+//      setState(() {
+//        homePageContent = val.toString();
+//      });
+//    });
+//    super.initState();
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +29,60 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text("百信生活+"),
         ),
-        body: SingleChildScrollView(
-          child: Text(
-            homePageContent
-          ),
+
+//        body: SingleChildScrollView(
+//          child: Text(
+//            homePageContent
+//          ),
+//        ),
+        body: FutureBuilder(
+          future: getHomePageContent(), // 异步方法
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var data = json.decode(snapshot.data.toString());
+              print("data============>" + data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(
+                    swipDataList: swiper,
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text('加载中'),
+              );
+            }
+          },
         ),
       ),
     );
   }
 }
 
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swipDataList;
 
+  // 接收数据
+  SwiperDiy({this.swipDataList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "${swipDataList[index]['image']}",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
+}
